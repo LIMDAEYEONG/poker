204b3ab41248bd341c9767a3222f9c0ee4c2f991
Sanghyeon Lee <isseebx38@gmail.com>
2017-04-24 09:56:52 +0900

    Add HandRanking class(enum)

---
+++ src/main/java/com/edu/cnu/poker/HandRanking.java
@@ -0,0 +1,23 @@
+package com.edu.cnu.poker;
+
+public enum HandRanking {
+    ROYAL_STRAIGHT_FLUSH(9),
+    STRAIGHT_FULSH(8),
+    FOUR_CARD(7),
+    FULL_HOUSE(6),
+    FULSH(5),
+    STRAIGHT(4),
+    TRIPLE(3),
+    TWO_PAIR(2),
+    ONE_PAIR(1),
+    NOTHING(0),
+
+    private int rankOfHand;
+
+    HandRanking(int rankOfHand) {
+        this.rankOfHand = rankOfHand;
+    }
+    public int getRankOfHand() {
+        return this.rankOfHand;
+    }
+}
