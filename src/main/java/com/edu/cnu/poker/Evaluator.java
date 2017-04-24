4c4aa169d48d73a2fa56525efb92d5e9479be420
Sanghyeon Lee <isseebx38@gmail.com>
2017-04-24 10:19:55 +0900

    apply HandRanking class to Evaluator and Test

--- src/main/java/com/edu/cnu/poker/Evaluator.java
+++ src/main/java/com/edu/cnu/poker/Evaluator.java
@@ -8,7 +8,7 @@ import java.util.Map;
  * Created by cse on 2017-04-17.
  */
 public class Evaluator {
-    public String evaluate(List<Card> cardList) {
+    public HandRanking evaluate(List<Card> cardList) {
         Map<Suit, Integer> tempMap = new HashMap<Suit, Integer>();
 
         for (Card card : cardList) {
@@ -23,9 +23,9 @@ public class Evaluator {
 
         for (Suit key : tempMap.keySet()) {
             if (tempMap.get(key) == 5) {
-                return "FLUSH";
+                return HandRanking.FULSH;
             }
         }
-        return "NOTHING";
+        return HandRanking.NOTHING;
     }
 }
